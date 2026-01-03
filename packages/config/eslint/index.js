463105c7990ed6e0b325fd3be659121ab7dd2f93
Aly <alydemah@gmail.com>
2026-01-03 14:15:00 +0100

    add shared config packages: typescript, eslint, tailwind

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
---
+++ packages/config/tailwind/index.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#f0f7ff",
+          100: "#e0effe",
+          200: "#bae0fd",
+          300: "#7cc8fb",
+          400: "#36adf6",
+          500: "#0c93e7",
+          600: "#0074c5",
+          700: "#015da0",
+          800: "#064f84",
+          900: "#0b436e",
+          950: "#072a49",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        mono: ["JetBrains Mono", "Fira Code", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
